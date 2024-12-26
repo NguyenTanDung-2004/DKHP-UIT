2b5ad383019dcbb3635e8a1b60d2ef2340768dbd
GiangPhanNguyenTra <22520360@gm.uit.edu.vn>
2024-12-26 18:03:38 +0700

    feat: add api get all open subject

--- DKHP_UIT/src/main/java/com/example/DKHP_UIT/controller/OpenSubjectController.java
+++ DKHP_UIT/src/main/java/com/example/DKHP_UIT/controller/OpenSubjectController.java
@@ -2,6 +2,7 @@ package com.example.DKHP_UIT.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +25,10 @@ public class OpenSubjectController {
     @PostMapping("/deleteOpenSubject")
     public ResponseEntity deleteOpenSubject(@RequestBody RequestOpenSubject requestOpenSubject) {
         return this.openSubjectService.deleteOpenSubject(requestOpenSubject);
+    }
+
+    @GetMapping("/getAllOpenSubject")
+    public ResponseEntity getAllOpenSubject() {
+        return this.openSubjectService.getAllOpenSubject();
     }
 }
